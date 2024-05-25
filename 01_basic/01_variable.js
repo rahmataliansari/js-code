48946ab223c6221a486234ddf0b67edd328a4706
Rahmat Ali <158486005+rahmataliansari@users.noreply.github.com>
2024-05-25 16:19:13 +0000

    container

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=5678
+let accountMail="rahmat@bb"
+var accountPasswors="12345"
+accountCity="patna"
+
+
+//accountId=2
+accountMail="rahmat@cc"
+accountPasswors="123456"
+/* we don't use var only use let and const
+*/
+
+console.log(accountId);
+console.table([accountMail, accountPasswors,])
